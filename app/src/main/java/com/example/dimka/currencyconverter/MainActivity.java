4409909d0a873e315c60fe74916065479d94b9c9
Dimka <MolodkovDO@gmail.com>
2016-11-09 21:46:00 +0200

    Add toolbar.

--- app/src/main/java/com/example/dimka/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/dimka/currencyconverter/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.dimka.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        initToolbar();
     }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+    }
+
+
 }
